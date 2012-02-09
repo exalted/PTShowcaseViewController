9d2388fef1c4ce69816107f74ad18c3721e08f3d
Ali Servet Donmez <asd@pittle.org>
2012-02-09 14:37:31 +0100

    Initial commit

---
+++ Examples/ShowcaseDemo/ShowcaseDemo/PTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PTAppDelegate.h
+//  ShowcaseDemo
+//
+//  Created by Ali Servet Donmez on 9.2.12.
+//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Examples/ShowcaseDemo/ShowcaseDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShowcaseDemo
+//
+//  Created by Ali Servet Donmez on 9.2.12.
+//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PTAppDelegate class]));
+    }
+}
