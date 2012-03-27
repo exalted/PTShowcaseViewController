e80d5133a261e527a037595ce901c3e46d18b91c
Ali Servet Donmez <asd@pittle.org>
2012-03-27 18:21:28 +0200

    Add copyright notice and statement of copying permission to each source file

--- Examples/ShowcaseDemo/ShowcaseDemo/main.m
+++ Examples/ShowcaseDemo/ShowcaseDemo/main.m
@@ -1,9 +1,19 @@
 //
-//  main.m
-//  ShowcaseDemo
+// Copyright (C) 2012 Ali Servet Donmez. All rights reserved.
 //
-//  Created by Ali Servet Donmez on 9.2.12.
-//  Copyright (c) 2012 Apex-net srl. All rights reserved.
+// This file is part of PTShowcaseViewController.
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PTShowcaseViewController is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PTShowcaseViewController. If not, see <http://www.gnu.org/licenses/>.
+// PTShowcaseViewController is free software: you can redistribute it and/or
 //
 
 #import <UIKit/UIKit.h>
